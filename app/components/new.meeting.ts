b0055009bbd36e21320509af9cfd8213f3095390
Alex <alex@roehms.de>
2016-07-21 16:39:01 +0200

    starting with date and time picker

--- app/components/new.meeting.ts
+++ app/components/new.meeting.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MeetingService } from '../services/meeting.backend';
 import { Meeting } from '../model/meeting.dtos';
+import { DatetimeEditor } from './edit.datetime';
 
 @Component({
     selector: 'meetme-new',
     templateUrl: './app/templates/new.meeting.view.html',
+    directives: [ DatetimeEditor ],
     providers: [
       MeetingService,
     ]
